35b5c285b017506c55722c873ca1decac2a4331f
Farkhod <dfarkhod@gmail.com>
2021-01-30 17:33:41 +0800

    added

---
+++ 13-dars/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public JsonResult Data()
+        {
+            return Json(new { id = 17, firstName = "Jaloliddin", lastName = "Rumiy" });
+        }
+
+
+    }
+}
