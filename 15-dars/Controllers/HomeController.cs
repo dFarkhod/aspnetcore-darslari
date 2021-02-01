0895c46c26ba7493b15fc149f624088af8fb8ea0
Farkhod <dfarkhod@gmail.com>
2021-02-01 17:59:52 +0800

    added 15th lesson files

---
+++ 15-dars/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StaffManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStaffRepository _staffRepository;
+
+        public HomeController(IStaffRepository staffRepository)
+        {
+            _staffRepository = staffRepository;
+        }
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public JsonResult Data()
+        {
+            return Json(new { id = 17, firstName = "Jaloliddin", lastName = "Rumiy" });
+        }
+
+        public string Staff()
+        {
+            return _staffRepository.Get(3)?.FirstName;
+        }
+
+
+    }
+}
