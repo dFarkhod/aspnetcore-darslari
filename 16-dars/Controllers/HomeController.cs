85056685e2acf2eb9cf86549d65472b5447ac3b1
Farkhod <dfarkhod@gmail.com>
2021-02-06 15:48:43 +0800

    next lesson's files

---
+++ 16-dars/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StaffManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStaffRepository _staffRepository;
+
+        public HomeController(IStaffRepository staffRepository)
+        {
+            _staffRepository = staffRepository;
+        }
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult ViewOne()
+        {
+            return View("View1");
+        }
+        public ViewResult ViewTwo()
+        {
+            return View("../Other/View2");
+        }
+        public ViewResult ViewThree()
+        {
+            return View("~/Top/View3.cshtml");
+        }
+
+        public JsonResult Data()
+        {
+            return Json(new { id = 17, firstName = "Jaloliddin", lastName = "Rumiy" });
+        }
+
+        public string Staff()
+        {
+            return _staffRepository.Get(3)?.FirstName;
+        }
+
+
+    }
+}
---
+++ 16-dars/Models/IStaffRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Models
+{
+    public interface IStaffRepository
+    {
+        Staff Get(int id);
+    }
+}
