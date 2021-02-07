a9df7f1c42d1ce791c9add376936db2c14be6582
Farkhod <dfarkhod@gmail.com>
2021-02-07 15:30:53 +0800

    17th lesson's files

---
+++ 17-dars/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StaffManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStaffRepository _staffRepository;
+
+        public HomeController(IStaffRepository staffRepository)
+        {
+            _staffRepository = staffRepository;
+        }
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Details()
+        {
+            Staff model = _staffRepository.Get(3);
+            ViewData["staff"] = model;
+            ViewBag.title = "Staff Details";
+            return View();
+        }
+        public string Staff()
+        {
+            return _staffRepository.Get(3)?.FirstName;
+        }
+    }
+}
