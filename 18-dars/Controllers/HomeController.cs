11edad59723e0b18e9bfd23c18a73377a24c5dc1
Farkhod <dfarkhod@gmail.com>
2021-02-12 12:01:46 +0800

    18-lesson source codes

---
+++ 18-dars/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StaffManagement.Models;
+using StaffManagement.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStaffRepository _staffRepository;
+
+        public HomeController(IStaffRepository staffRepository)
+        {
+            _staffRepository = staffRepository;
+        }
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Details()
+        {
+            HomeDetailsViewModel viewModel = new HomeDetailsViewModel()
+            {
+                Staff = _staffRepository.Get(3),
+                Title = "Staff Details"
+            };
+
+            return View(viewModel);
+        }
+        public string Staff()
+        {
+            return _staffRepository.Get(3)?.FirstName;
+        }
+    }
+}
---
+++ 18-dars/Models/MockStaffRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Models
+{
+    public class MockStaffRepository : IStaffRepository
+    {
+        private List<Staff> _staffs = null;
+
+        public MockStaffRepository()
+        {
+            _staffs = new List<Staff>()
+            {
+                new Staff() {Id = 1, FirstName = "Malik", LastName = "Shox", Email = "usmon@virtualdars.com", Department="Admin" },
+                new Staff() {Id = 2, FirstName = "Salohiddin", LastName = "Ayyubiy", Email = "usmon@virtualdars.com", Department="Production" },
+                new Staff() {Id = 3, FirstName = "Usmon", LastName = "G'oziy", Email = "usmon@virtualdars.com", Department="R&D" }
+            };
+        }
+        public Staff Get(int id)
+        {
+            return _staffs.FirstOrDefault(staff => staff.Id.Equals(id));
+        }
+    }
+}
