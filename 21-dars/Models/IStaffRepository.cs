e6d17febf1f263f721f984dfa0fd056b8e192252
Farkhod <dfarkhod@gmail.com>
2021-02-21 19:01:27 +0800

    next lesson fiels

---
+++ 21-dars/Models/IStaffRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Models
+{
+    public interface IStaffRepository
+    {
+        Staff Get(int id);
+
+        IEnumerable<Staff> GetAll();
+    }
+}
