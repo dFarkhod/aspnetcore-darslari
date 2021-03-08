4057701e8b6a34c9ae8daa96cf45eefff33754f0
Farkhod <dfarkhod@gmail.com>
2021-03-08 18:52:22 +0800

    26-dars

---
+++ 26-dars/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StaffManagement.Models;
+using StaffManagement.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStaffRepository _staffRepository;
+
+        public HomeController(IStaffRepository staffRepository)
+        {
+            _staffRepository = staffRepository;
+        }
+
+        public ViewResult Index()
+        {
+            HomeIndexViewModel viewModel = new HomeIndexViewModel()
+            {
+                Staffs = _staffRepository.GetAll()
+            };
+            return View(viewModel);
+        }
+
+        public ViewResult Details(int? id)
+        {
+            HomeDetailsViewModel viewModel = new HomeDetailsViewModel()
+            {
+                Staff = _staffRepository.Get(id??1),
+                Title = "Staff Details"
+            };
+
+            return View(viewModel);
+        }
+
+    }
+}
