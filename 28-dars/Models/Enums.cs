662e6d05f911ef688915bba4a405fb7957c2a06c
Farkhod <dfarkhod@gmail.com>
2021-03-10 15:22:30 +0800

    28-dars

---
+++ 28-dars/Models/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Models
+{
+    public enum Department
+    {
+        None,
+        Admin,
+        Production,
+        RnD,
+        Sales,
+        Marketing
+    }
+}
---
+++ 28-dars/Models/Staff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Models
+{
+    public class Staff
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public Department Department { get; set; }
+    }
+}
