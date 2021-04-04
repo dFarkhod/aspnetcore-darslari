058f529abc6de53033a16f20b70e0107ac30449a
Farkhod <dfarkhod@gmail.com>
2021-04-04 20:50:39 +0800

    added 35 lesson codes

---
+++ 35-dars/StaffManagement.DataAccess/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StaffManagement.DataAccess.Models;
+using StaffManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaffManagement.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Staff> Staffs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Seed();
+        }
+    }
+}
---
+++ 35-dars/StaffManagement.DataAccess/Models/SqlserverStaffRepository.cs
@@ -0,0 +1,52 @@
+﻿using StaffManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaffManagement.DataAccess.Models
+{
+    public class SqlserverStaffRepository : IStaffRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public SqlserverStaffRepository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        Staff IStaffRepository.Create(Staff staff)
+        {
+            dbContext.Staffs.Add(staff);
+            dbContext.SaveChanges();
+            return staff;
+        }
+
+        Staff IStaffRepository.Delete(int id)
+        {
+            var staff = dbContext.Staffs.Find(id);
+            if (staff != null)
+            {
+                dbContext.Staffs.Remove(staff);
+                dbContext.SaveChanges();
+            }
+            return staff;
+        }
+
+        Staff IStaffRepository.Get(int id)
+        {
+           return dbContext.Staffs.Find(id);
+        }
+
+        IEnumerable<Staff> IStaffRepository.GetAll()
+        {
+           return dbContext.Staffs;
+        }
+
+        Staff IStaffRepository.Update(Staff updateStaff)
+        {
+            var staff = dbContext.Staffs.Attach(updateStaff);
+            staff.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            dbContext.SaveChanges();
+            return updateStaff;
+        }
+    }
+}
