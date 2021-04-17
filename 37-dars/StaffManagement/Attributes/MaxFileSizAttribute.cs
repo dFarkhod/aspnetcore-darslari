cbf1480e5715cc88b962a002b926f8f796f67a6c
Farkhod <dfarkhod@gmail.com>
2021-04-17 13:27:18 +0800

    lesson 37

---
+++ 37-dars/StaffManagement/Attributes/MaxFileSizAttribute.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Attributes
+{
+    public class MaxFileSizeAttribute : ValidationAttribute
+    {
+        private readonly int _maxFileSizeKB;
+        private readonly int _maxFileSize;
+
+        public MaxFileSizeAttribute(int maxFileSizeInKB)
+        {
+            _maxFileSize = maxFileSizeInKB;
+            _maxFileSizeKB = _maxFileSize * 1024;
+        }
+
+        protected override ValidationResult IsValid(
+        object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                if (file.Length > _maxFileSizeKB)
+                {
+                    return new ValidationResult($"The file size exceeds the limit allowed { _maxFileSize} KB.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ 37-dars/StaffManagement/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using StaffManagement.Models;
+using StaffManagement.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStaffRepository _staffRepository;
+        private readonly IWebHostEnvironment webHost;
+
+        public HomeController(IStaffRepository staffRepository, IWebHostEnvironment webHost)
+        {
+            _staffRepository = staffRepository;
+            this.webHost = webHost;
+        }
+
+        public ViewResult Index()
+        {
+            HomeIndexViewModel viewModel = new HomeIndexViewModel()
+            {
+                Staffs = _staffRepository.GetAll()
+            };
+            return View(viewModel);
+        }
+
+        public ViewResult Details(int? id)
+        {
+            HomeDetailsViewModel viewModel = new HomeDetailsViewModel()
+            {
+                Staff = _staffRepository.Get(id??1),
+                Title = "Staff Details"
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(HomeCreateViewModel staff)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = string.Empty;
+                if (staff.Photo != null)
+                {
+                    string uploadFolder = Path.Combine(webHost.WebRootPath, "images");
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + staff.Photo.FileName;
+                    string imageFilePath = Path.Combine(uploadFolder, uniqueFileName);
+                    staff.Photo.CopyTo(new FileStream(imageFilePath, FileMode.Create));
+                }
+
+                Staff newStaff = new Staff
+                {
+                    FirstName = staff.FirstName,
+                    LastName = staff.LastName,
+                    Email = staff.Email,
+                    Department = staff.Department,
+                    PhotoFilePath = uniqueFileName
+                };
+
+                newStaff = _staffRepository.Create(newStaff);
+                return RedirectToAction("details", new { id = newStaff.Id });
+            }
+
+            return View();
+        }
+
+    }
+}
