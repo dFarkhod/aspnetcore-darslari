cd509486f7984b12ab55c42511912d84e1802632
Farkhod <dfarkhod@gmail.com>
2021-04-25 15:53:19 +0800

    next lesson

---
+++ 40-dars/StaffManagement/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            if (statusCode == 404)
+            {
+                ViewBag.ErrorMessage = "The resource you are looking for is not found";
+            }
+            return View("NotFound");
+        }
+    }
+}
---
+++ 40-dars/StaffManagement/ViewModels/HomeEditViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffManagement.ViewModels
+{
+    public class HomeEditViewModel : HomeCreateViewModel
+    {
+        public string ExistingPhotoFilePath { get; set; }
+    }
+}
